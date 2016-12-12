4ea2e36d028c87903493149667facbc1842b13f5
nikkifang <nikkifang@tencent.com>
2016-12-12 21:13:12 +0800

    style modify

--- src/js/morphing.js
+++ src/js/morphing.js
@@ -55,5 +55,28 @@ $("#js-svg-morpheus").on("click",function () {
     });
     morpheusTest.to("dog");
 });
+
+(function () {
+    var paper_menu = new Snap("#menu");
+    var menu_path = paper_menu.select("#js-svg-menu-path");
+    var is_open = false;
+    var pathDataOpen = "M158.5,0H0v53.1c0,0,19.6-4.6,66-0.2s60.5-3.8,92.5-0.1V0z";
+    var pathDataTrans = "M158.5,0H0v53.1c0,0,35.4,15.4,82,13.8s76.5-14.1,76.5-14.1V0z";
+    var pathDataClose = "M158.5,0H0v55.6c20.9-12.8,38.5,19.5,73.5-1.9s73.2-7.2,85,0V0z";
+    $("#js-menu").on("click",function () {
+        if(!is_open){
+            menu_path.stop().animate({d: pathDataTrans},320,mina.easeinout,function () {
+                menu_path.stop().animate({d: pathDataOpen},2500,mina.elastic);
+            });
+            is_open = !is_open;
+        }else {
+            menu_path.stop().animate({d: pathDataClose},3000,mina.elastic);
+            is_open = !is_open;
+        }
+        $("#menu").toggleClass("menu--open");
+    });
+})();
+
+
 startMorph();
 
